89b74c031177a4b03cb082c29d9ac48c09b90491
kolikler <bariskaya3@hotmail.com>
2021-03-25 14:19:01 +0300

    barış kaya

--- app/src/main/java/com/semanureserler/mobilprojem1/MainActivity.java
+++ app/src/main/java/com/semanureserler/mobilprojem1/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int a = 5;
+        int b=10 ; // baris ekledi
         getActionBar();
     }
 }
