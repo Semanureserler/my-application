185ed2b7d16e24d82b4c0ccf7cda80c565ca2a4e
kolikler <bariskaya3@hotmail.com>
2021-03-28 13:15:23 +0300

    barış kaya

--- app/src/main/java/com/semanureserler/mobilprojem1/MainActivity.java
+++ app/src/main/java/com/semanureserler/mobilprojem1/MainActivity.java
@@ -2,9 +2,14 @@ package com.semanureserler.mobilprojem1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button anasayfayagit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
         int a = 5;
         int b=10 ; // baris ekledi
         getActionBar();
+
+        anasayfayagit =findViewById(R.id.button);
+        anasayfayagit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent anasayfa= new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(anasayfa);
+            }
+        });
     }
 }
